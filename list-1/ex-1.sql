7bc23937cecbffcb8d11fca867f6b3f8108a449e
Maciej Buszka <maciej@buszka.eu>
2020-03-11 14:29:15 +0100

    upload all changes

--- list-1/ex-1.sql
+++ list-1/ex-1.sql
@@ -1,16 +1,18 @@
-WITH the_semester AS (
-  SELECT semestr_id FROM semestr WHERE nazwa = 'Semestr zimowy 2010/2011'
-),
+WITH 
+  the_semester AS (
+    SELECT semestr_id FROM semestr WHERE nazwa = 'Semestr zimowy 2010/2011'
+  ),
   the_przedmiot AS (
     SELECT kod_przed FROM przedmiot WHERE nazwa = 'Matematyka dyskretna (M)'
   ),
-proxy AS (
-  SELECT *
-  FROM
-    grupa natural join the_przedmiot
-          natural join przedmiot_semestr
-          natural join the_semester
-          natural join uzytkownik
-  WHERE rodzaj_zajec in ('C', 'c')
-  ORDER BY nazwisko
-) SELECT string_agg(proxy.nazwisko, ',') FROM proxy
+  proxy AS (
+    SELECT *
+    FROM
+      grupa natural join the_przedmiot
+            natural join przedmiot_semestr
+            natural join the_semester
+            natural join uzytkownik
+    WHERE rodzaj_zajec in ('C', 'c')
+    ORDER BY nazwisko
+  )
+  SELECT string_agg(proxy.nazwisko, ',') FROM proxy
